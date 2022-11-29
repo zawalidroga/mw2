6e01703fefff0e85c3de904954d65be13bcbbeaf
Zawalidroga <michal.walo.8@gmail.com>
2022-11-29 13:32:26 +0100

    finish converting to fiber three js

--- src/components/blop.js
+++ src/components/blop.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import gsap from "gsap";
 import { createNoise3D } from "simplex-noise";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useFrame } from "@react-three/fiber";
 
 
@@ -9,11 +9,15 @@ const noise3d = createNoise3D();
 const particlesNumber = 6000;
 
 export const Blop = () => {
+    const [rotationX, setRotationX] = useState(0);
+    const [rotationY, setRotationY] = useState(0);
+
+
     const vertices = new Float32Array(particlesNumber * 3);
     const distance = Math.min(200, window.innerWidth / 4);
     
-
-      const ref = useRef(null);
+    const refPoints = useRef(null)
+    const ref = useRef(null);
 
 useEffect(() => {
     ref.current.setAttribute( 'position', new THREE.Float32BufferAttribute(vertices, 3));
@@ -41,14 +45,17 @@ useEffect(() => {
     }
   }, []);
 
+
+
   useFrame(() => {
+
     const time = performance.now() * 0.5;
     
     const basePositionAttribute = ref.current.getAttribute('basePosition');
     const positionAttribute = ref.current.getAttribute('position');
     const vertex = new THREE.Vector3();
 
-    console.log(basePositionAttribute)
+    //console.log(basePositionAttribute)
 if(basePositionAttribute !== undefined){ 
     for (var i = 0; i < particlesNumber; i++) {
       vertex.fromBufferAttribute(basePositionAttribute, i);
@@ -62,14 +69,25 @@ if(basePositionAttribute !== undefined){
       let ratio = noise * 0.4 * (0.8 + 0.1) + 0.8;
       vertex.multiplyScalar(ratio);
 
+        // setVertexo(() => [])
+        // console.log(vertexo)
       positionAttribute.setXYZ(i, vertex.x, vertex.y, vertex.z);
+
     }
 
+    setRotationX((rotationX) => rotationX + 0.003);
+    setRotationY((rotationY) => rotationY + 0.003);
+    //refPoints.current.rotation.set(rotationX, rotationY)   
+    refPoints.current.rotation.x = rotationX;
+    refPoints.current.rotation.y = rotationY;
+    
+    refPoints.current.geometry.attributes.position.needsUpdate = true;
+    refPoints.current.geometry.computeBoundingSphere();
   }});
 
   return (
     <>
-      <points boundingSphere={50} >
+      <points boundingSphere={50} ref={refPoints}>
         <bufferGeometry
             ref={ref}
             attach = 'geometry'
